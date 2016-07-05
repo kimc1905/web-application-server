5ace6d1eff14caee9a07c0c9db5ce09b1a41ddec
kimc1905 <kimc1905@naver.com>
2016-07-06 01:17:07 +0900

    Pair 클래스 lombok 사용

--- src/main/java/util/HttpRequestUtils.java
+++ src/main/java/util/HttpRequestUtils.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 public class HttpRequestUtils {
 
@@ -85,6 +87,7 @@ public class HttpRequestUtils {
 		return getKeyValue(header, ": ");
 	}
 
+    @Data
     public static class Pair {
 		String key;
 		String value;
@@ -92,50 +95,6 @@ public class HttpRequestUtils {
 		Pair(String key, String value) {
 			this.key = key.trim();
 			this.value = value.trim();
-		}
-		
-		public String getKey() {
-			return key;
-		}
-		
-		public String getValue() {
-			return value;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((key == null) ? 0 : key.hashCode());
-			result = prime * result + ((value == null) ? 0 : value.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			Pair other = (Pair) obj;
-			if (key == null) {
-				if (other.key != null)
-					return false;
-			} else if (!key.equals(other.key))
-				return false;
-			if (value == null) {
-				if (other.value != null)
-					return false;
-			} else if (!value.equals(other.value))
-				return false;
-			return true;
-		}
-
-		@Override
-		public String toString() {
-			return "Pair [key=" + key + ", value=" + value + "]";
 		}
 	}
 }
